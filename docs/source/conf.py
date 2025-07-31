0fbec9959c6eee9c4ec4b893afc2fc0c92d4b9b1
FlTixier <82396751+FlTixier@users.noreply.github.com>
2025-07-31 10:16:16 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,8 +13,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../src'))
-import feature_harmonization
-import feature_normalization
+#import feature_harmonization
+#import feature_normalization
 import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
